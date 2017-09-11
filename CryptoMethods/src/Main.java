8ec9a86e78ab9eb82489c1d6f3c160671232ff1e
Tuseeva Elena <etuseeva@griddynamics.com>
2017-09-11 20:30:37 +0300

    ...

--- CryptoMethods/src/Main.java
+++ CryptoMethods/src/Main.java
@@ -1,3 +1,5 @@
+import com.sun.org.glassfish.gmbal.ManagedAttribute;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -11,7 +13,9 @@ public class Main {
 
     public static void main(String[] args) {
         List<String> input = readFile("input.txt");
-        numbs(input);
+//        numbs(input);
+
+        polynomials(input);
     }
 
     private static void numbs(List<String> input) {
@@ -69,12 +73,169 @@ public class Main {
         }
     }
 
-    private static void polygons(List<String> input) {
-        int[] a, b;
+    static class Polynomial {
+        private int[] c;
+        private int deg;
+
+        Polynomial(int[] c) {
+            this.c = c;
+            this.deg = c.length + 1;
+        }
+
+        static int[] read(String str) {
+            String[] nums = str.split(" ");
+
+            int[] a = new int[nums.length];
+            for (int i = 0; i < nums.length; i++) {
+                a[i] = Integer.parseInt(nums[i]);
+            }
+
+            return a;
+        }
+
+        static int[] sum(int a[], int b[]) {
+            int maxLen = Math.max(a.length, b.length), minLen = Math.min(a.length, b.length);
+
+            int[] c = new int[maxLen];
+            for (int i = 0; i < minLen; i++) {
+                c[i] = a[i] + b[i];
+            }
+
+            if (a.length < b.length) {
+                System.arraycopy(b, minLen, c, minLen, maxLen - minLen);
+            } else {
+                System.arraycopy(a, minLen, c, minLen, maxLen - minLen);
+            }
+
+            return c;
+        }
+
+        static int[] multConst(int a[], int x) {
+            int c[] = new int[a.length];
+            for (int i = 0; i < a.length; i++) {
+                c[i] = a[i] * x;
+            }
+            return c;
+        }
+
+        static int[] divConst(int a[], int x) {
+            int c[] = new int[a.length];
+            for (int i = 0; i < a.length; i++) {
+                c[i] = a[i] / x;
+            }
+            return c;
+        }
+
+        static int[] diff(int a[], int b[]) {
+            int maxLen = Math.max(a.length, b.length), minLen = Math.min(a.length, b.length);
+
+            int[] c = new int[maxLen];
+            for (int i = 0; i < minLen; i++) {
+                c[i] = a[i] - b[i];
+            }
+
+            if (a.length < b.length) {
+                System.arraycopy(b, minLen, c, minLen, maxLen - minLen);
+            } else {
+                System.arraycopy(a, minLen, c, minLen, maxLen - minLen);
+            }
+
+            return c;
+        }
+
+        static int[] divX(int a[]) {
+            int j = 0;
+            while (a[j] == 0) j++;
+
+            int[] c = new int[a.length - j];
+            int k = 0;
+            for (int i = j; i < a.length; i++) {
+                c[k++] = a[i];
+            }
+
+            return c;
+        }
+
+        static int[] divOnceX(int a[]) {
+            if (a[0] == 0) {
+                int[] c = new int[a.length - 1];
+                System.arraycopy(a, 1, c, 0, a.length - 1);
+                return c;
+            }
+
+            return a;
+        }
+
+        static int[] multOnceX(int a[]) {
+            int[] c = new int[a.length + 1];
+            c[0] = 0;
+            System.arraycopy(a, 0, c, 1, a.length);
+            return c;
+        }
+
+        static int[] removeZeros(int a[]) {
+            int j = a.length - 1;
+            while (a[j] == 0) j--;
+            int[] c = new int[j + 1];
+            System.arraycopy(a, 0, c, 0, j + 1);
+            return c;
+        }
+
+        static int[] mult(int a[], int b[]) {
+            int[] c = new int[a.length + b.length];
+
+//            for (int k = 0; k < ?; k++) {
+//                for (int )
+//            }
+
+            return c;
+        }
+
     }
 
-    private static int polygonGcd(int[] a, int[] b) {
-        return 0;
+    private static void polynomials(List<String> input) {
+        int[] a = Polynomial.read(input.get(0)), b = Polynomial.read(input.get(1));
+
+        System.out.println(Arrays.toString(Polynomial.removeZeros(b)));
+//        System.out.println(polynomialGcd(a, b));
+    }
+
+    private static int[] polynomialGcd(int[] a, int[] b) {
+        int[] g = new int[]{1};
+
+        while (a[0] == 0 && b[0] == 0) {
+            a = Polynomial.divOnceX(a);
+            b = Polynomial.divOnceX(b);
+            g = Polynomial.multOnceX(g);
+        }
+
+        int[] u = a, v = b;
+
+        while (u.length != 1) {
+            u = Polynomial.divOnceX(a);
+            v = Polynomial.divOnceX(b);
+
+            u = Polynomial.removeZeros(u);
+            v = Polynomial.removeZeros(v);
+
+            int t = v[0], k = u[0];
+            int[] d = Polynomial.diff(Polynomial.multConst(u, t), Polynomial.multConst(v, k));
+
+            int e = 0;
+            for (int i = 0; i < d.length; i++) {
+                e = gcd(e, a[i]);
+            }
+
+            int[] res = Polynomial.divConst(d, e);
+
+            if (u.length > v.length) {
+                u = res;
+            } else {
+                v = res;
+            }
+        }
+
+        return Polynomial.mult(g, v);
     }
 
     private static List<String> readFile(String file) {
