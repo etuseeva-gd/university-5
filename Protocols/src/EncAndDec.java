f9e794b662609b2460d17aab0203ad9510375e27
Tuseeva Elena <etuseeva@griddynamics.com>
2017-09-20 11:10:36 +0300

    ...

---
+++ Protocols/src/EncAndDec.java
@@ -0,0 +1,18 @@
+public class EncAndDec {
+    class Keys {
+        String privateKey;
+        String publicKey;
+
+        public void genKeys() {
+
+        }
+    }
+
+    public static String encrypt(Keys keys) {
+        return "";
+    }
+
+    public static String decrypt(Keys keys) {
+        return "";
+    }
+}
--- Protocols/src/Main.java
+++ Protocols/src/Main.java
@@ -1,7 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner in = new Scanner(System.in);
 
+        System.out.println("Введите число игроков (не больше 10)");
+        int n = in.nextInt();
 
-    public static void main(String[] args) {
+        if (n < 0 || n > 10) {
+            throw new Exception("Не верное число участников.");
+        }
+
+        in.nextLine();
+        System.out.println("Ведите имена игроков, раздающего пометьте символом *");
+
+        User[] users = new User[n];
+        for (int i = 0; i < n; i++) {
+            String line = in.nextLine();
+            users[i] = new User(line.contains("*"), line);
+        }
+        System.out.println(Arrays.toString(users));
 
     }
 }
