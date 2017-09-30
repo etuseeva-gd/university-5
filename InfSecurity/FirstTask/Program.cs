ebe503a12dd7b3cce9d9a8d943ade54442e5a1db
lena <lenok-cotik@yandex.ru>
2017-09-30 15:32:58 +0300

    ...

--- InfSecurity/FirstTask/Program.cs
+++ InfSecurity/FirstTask/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Management;
-using System.Runtime.InteropServices;
 using System.Text;
 
 namespace FirstTask
@@ -25,87 +24,149 @@ namespace FirstTask
     internal class Program
     {
         static long ONE_GB = 1073741824;
-        
-        const int PROCESS_WM_READ = 0x0010;
-        const int PROCESS_VM_WRITE = 0x0020;
-        const int PROCESS_VM_OPERATION = 0x0008;
-
-        [DllImport("kernel32.dll")]
-        public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
-
-        [DllImport("kernel32.dll")]
-        public static extern bool ReadProcessMemory(int hProcess, 
-            int lpBaseAddress, byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesRead);
-        
-        [DllImport("kernel32.dll", SetLastError = true)]
-        static extern bool WriteProcessMemory(int hProcess, int lpBaseAddress, 
-            byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesWritten);
+        public static string s2 = "B\0e\0g\0i\0e\0S\0e\0a\0c\0h\0e\0r\0q\0q\0q\0";
+        public static string s = "BegieSeacherqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq";
         
         public static void Main(string[] args)
         {
-            try
+            Drive drive = GetCurrentDrive(GetDrives());
+            Console.WriteLine("Current hard drive: " + drive);
+
+            if (FindPosition("FirstTask.exe") != -1)
             {
-                List<Drive> drives = new List<Drive>();
-                foreach (ManagementObject drive in new ManagementObjectSearcher("SELECT * FROM Win32_DiskDrive").Get())
-                {
-                    var d = new Drive
-                    {
-                        Model = (string) drive["Model"],
-                        SerialNumber = (string) drive["SerialNumber"],
-                        FirmwareRevision = (string) drive["FirmwareRevision"],
-                        Size = long.Parse(drive["Size"].ToString()) / ONE_GB
-                    };
-
-                    foreach (ManagementObject partition in new ManagementObjectSearcher(
-                        "ASSOCIATORS OF {Win32_DiskDrive.DeviceID='" + drive["DeviceID"]
-                        + "'} WHERE AssocClass=Win32_DiskDriveToDiskPartition").Get())
-                    {
-                        foreach (ManagementObject disk in new ManagementObjectSearcher(
-                            "ASSOCIATORS OF {Win32_DiskPartition.DeviceID='" + partition["DeviceID"]
-                            + "'} WHERE AssocClass=Win32_LogicalDiskToPartition").Get())
-                        {
-                            d.Name = (string) disk["Name"];
-                        }
-                    }
-                    drives.Add(d);
-                }
-
-                foreach (var d in drives)
-                {
-                    Console.WriteLine(d.ToString());
-                }
-                
-               
-             
-                string root = Path.GetPathRoot(System.Reflection.Assembly.GetEntryAssembly().Location);
-                Console.WriteLine(root);
-
-                int id = Process.GetCurrentProcess().Id;
-                
-                 
-                
-                Console.WriteLine(Process.GetCurrentProcess().Id);
+                RunExe();
+                Item(drive);
+                CreateBatFile();
             }
-            catch (Exception e)
+            else
             {
-               Console.WriteLine(e.Message);
+                Console.WriteLine(CheckHard(drive) ? "Application is working" : "Application can't work");
+                Console.Read();
+            }
+        }
+
+        private static Drive GetCurrentDrive(List<Drive> drives)
+        {
+            string BaseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            BaseDirectory = (BaseDirectory.Remove(BaseDirectory.IndexOf("\\")));
+            
+            foreach (var drive in drives)
+            {
+                if (BaseDirectory == drive.Name)
+                {
+                    return drive;
+                }
+            }
+            return null;
+        }
+
+        private static List<Drive> GetDrives()
+        {
+            var drives = new List<Drive>();
+            foreach (ManagementObject drive in
+                new ManagementObjectSearcher("SELECT * FROM Win32_DiskDrive").Get())
+            {
+                var d = new Drive
+                {
+                    Model = (string) drive["Model"],
+                    SerialNumber = (string) drive["SerialNumber"],
+                    FirmwareRevision = (string) drive["FirmwareRevision"],
+                    Size = long.Parse(drive["Size"].ToString()) / ONE_GB
+                };
+
+                foreach (ManagementObject partition in new ManagementObjectSearcher(
+                    "ASSOCIATORS OF {Win32_DiskDrive.DeviceID='" + drive["DeviceID"]
+                    + "'} WHERE AssocClass=Win32_DiskDriveToDiskPartition").Get())
+                foreach (ManagementObject disk in new ManagementObjectSearcher(
+                    "ASSOCIATORS OF {Win32_DiskPartition.DeviceID='" + partition["DeviceID"]
+                    + "'} WHERE AssocClass=Win32_LogicalDiskToPartition").Get())
+                    d.Name = (string) disk["Name"];
+                drives.Add(d);
             }
 
-            /*Process process = Process.GetProcessesByName("Microsoft.Photos")[0];
-            Console.WriteLine(process);
-            
-            IntPtr processHandle = OpenProcess(PROCESS_WM_READ, false, process.Id); 
-            
-            int bytesWritten = 0;
-            byte[] buffer = Encoding.Unicode.GetBytes("It works!\0");
-            
-          // WriteProcessMemory((int)processHandle, 0xdd, buffer, buffer.Length, ref bytesWritten);
+            return drives;
+        }
+    
+        private static int FindPosition(string name = "FirstTask2.exe")
+        {
+            using (StreamReader read = new StreamReader(File.Open(name, FileMode.Open, FileAccess.Read, FileShare.Read), Encoding.Default))
+            {
+                string row = read.ReadToEnd();
+                return row.IndexOf(s2);
+            }
+        }
+        
+        private static void CreateBatFile()
+        {
+            using (FileStream fstream = new FileStream(@"batFile.bat", FileMode.OpenOrCreate))
+            {
+                string[] words = new string[5];
+                words[0] = "ping -n 1 -w 500 127.0.0.1 >nul" + Environment.NewLine;
+                words[1] = "del FirstTask.exe /q /s" + Environment.NewLine;
+                words[2] = "ren FirstTask2.exe FirstTask.exe" + Environment.NewLine;
+                words[3] = "start FirstTask.exe" + Environment.NewLine;
+                words[4] = "del %0";
 
-            int bytesRead = 0;
-            buffer = new byte[24];
-            ReadProcessMemory((int)processHandle, 0xee, buffer, buffer.Length, ref bytesRead);
+                foreach (string item in words)
+                {
+                    byte[] array = System.Text.Encoding.Default.GetBytes(item);
+                    fstream.Write(array, 0, array.Length);
+                }
+            }
+            Process.Start(@"batFile.bat");
+        }
+        
+        private static void Item(Drive drive)
+        {
+            var peek = FindPosition();
+            
+            var row = drive.Model + '@' + drive.SerialNumber + '@' + drive.FirmwareRevision + '@' + drive.Size;
+            var masByte = GetRowByte(row);
 
-            Console.WriteLine(Encoding.Unicode.GetString(buffer) + " (" + bytesRead + "bytes)");*/
+            using (var writer = new BinaryWriter(File.Open("FirstTask2.exe", FileMode.Open, FileAccess.Write), Encoding.Default))
+            {
+                writer.Seek(peek, SeekOrigin.Begin);
+                writer.Write(masByte, 0, masByte.Length);
+            }
+        }
+        
+        private static bool CheckHard(Drive drive)
+        {
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                if (s[i] == 'q')
+                    s = s.Remove(i);
+            }
+            string[] ss = s.Split(new char[] { '@' }, StringSplitOptions.RemoveEmptyEntries);
+            
+            Console.Write("File can be executed on: ");
+            foreach (var s1 in ss)
+            {
+                Console.Write(s1 + " ");
+            }
+            Console.WriteLine();
+            
+            return ss[0] == drive.Model && ss[1] == drive.SerialNumber && ss[2] == drive.FirmwareRevision && ss[3] == drive.Size + "";
+        }
+        
+        private static byte[] GetRowByte(string row)
+        {
+            char[] masChar = row.Normalize().ToCharArray();
+            byte[] masByte = new byte[masChar.Length * 2];
+            int k = 0;
+            for (int i = 0; i < masChar.Length; i++)
+            {
+                masByte[k++] = (byte)masChar[i];
+                masByte[k++] = 0;
+            }
+            return masByte;
+        }
+        
+        private static void RunExe()
+        {
+            if (File.Exists("FirstTask2.exe"))
+                File.Delete("FirstTask2.exe");
+            File.Copy("FirstTask.exe", "FirstTask2.exe");
         }
     }
 }
