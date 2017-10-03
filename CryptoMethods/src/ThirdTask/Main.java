562fe1087066ff6519641ace25494868d2cd0a08
Tuseeva Elena <etuseeva@griddynamics.com>
2017-10-03 16:31:51 +0300

    ..

--- CryptoMethods/src/ThirdTask/Main.java
+++ CryptoMethods/src/ThirdTask/Main.java
@@ -1,6 +1,7 @@
 package ThirdTask;
 
 import java.text.MessageFormat;
+import java.util.ArrayList;
 import java.util.List;
 
 import static FirstTask.Main.outToFile;
@@ -9,16 +10,75 @@ import static FirstTask.Main.readFromFile;
 public class Main {
 
     static int getLegendreSymbol(int a, int p) {
-        if (a == 0) {
-            return 0;
-        }
-        if (a == 1) {
-            return 1;
+        if (a == 0 || a == 1) {
+            return a;
         }
         if (a % 2 == 0) {
             return (int) (getLegendreSymbol(a / 2, p) * Math.pow(-1, (p * p - 1) / 8));
         } else {
             return (int) (getLegendreSymbol(p % a, a) * Math.pow(-1, (a - 1) * (p - 1) / 4));
+        }
+    }
+
+    static boolean isPrime(int num) {
+        if (num < 2) return false;
+        if (num == 2) return true;
+        if (num % 2 == 0) return false;
+        for (int i = 3; i * i <= num; i += 2)
+            if (num % i == 0) return false;
+        return true;
+    }
+
+    static List<Integer> factorize(int n) {
+        List<Integer> f = new ArrayList<>();
+
+        int p = 2;
+        while (true) {
+            while (n % p == 0 && n > 0) {
+                f.add(p);
+                n = n / p;
+            }
+            p++;
+            if (p > n / p) {
+                break;
+            }
+        }
+
+        if (n > 1) {
+            f.add(n);
+        }
+
+        return f;
+    }
+
+    static int legendre(int a, int p) {
+        if (a >= p || a < 0) {
+            return legendre(a % p, p);
+        } else if (a == 1 || a == 0) {
+            return a;
+        } else if (a == 2) {
+            if (p % 8 == 1 || p % 8 == 7) {
+                return 1;
+            } else {
+                return -1;
+            }
+        } else if (a == p - 1) {
+            if (p % 4 == 1) {
+                return 1;
+            } else {
+                return -1;
+            }
+        } else if (!isPrime(a)) {
+            List<Integer> factors = factorize(a);
+            int pr = 1;
+            for (int factor : factors) {
+                pr *= legendre(factor, p);
+            }
+            return pr;
+        } else if (((p - 1) / 2) % 2 == 0 || ((a - 1) / 2) % 2 == 0) {
+            return legendre(p, a);
+        } else {
+            return -1 * legendre(p, a);
         }
     }
 
@@ -29,6 +89,6 @@ public class Main {
         int a = Integer.parseInt(nums[0]),
                 p = Integer.parseInt(nums[1]);
 
-        outToFile(MessageFormat.format("L({0}, {1}) = {2}", a, p, getLegendreSymbol(a, p)));
+        outToFile(MessageFormat.format("L({0}, {1}) = {2}", a, p, legendre(a, p)));
     }
 }
