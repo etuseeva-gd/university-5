fb4ebd28b8efb572c2ab6813e42674b95c1d2ea0
Tuseeva Elena <etuseeva@griddynamics.com>
2017-10-06 14:18:39 +0300

    ...

--- Protocols/src/MentalPoker/Main.java
+++ Protocols/src/MentalPoker/Main.java
@@ -1,20 +1,7 @@
 package MentalPoker;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Введите в файл input.txt имена участников (их количество > 1 && < 6)");
-        System.out.println("Одна строка - один участник");
-        System.out.println("1 строка - участник будет считаться организатором");
-
-        System.out.println("Вы заполнили input.txt? (Да - 1, Нет - 0)");
-        Scanner scanner = new Scanner(System.in);
-        if (scanner.nextInt() == 0) {
-            System.out.println("Заполните файл и запусте снова");
-            return;
-        }
-
         new Protocol().init();
     }
 }
--- Protocols/src/MentalPoker/Protocol.java
+++ Protocols/src/MentalPoker/Protocol.java
@@ -18,42 +18,54 @@ public class Protocol {
 
         User(String name, BigInteger p) throws NoSuchAlgorithmException {
             this.name = name;
-            this.keys = new CryptoSystem.Keys(p);
+            this.keys = genKeys(p);
         }
 
-        private StringBuilder encrypt(List<String> cards, Boolean isCardsStr) {
+        User(String name) {
+            List<String> r = Transport.read("!" + name + "_params.txt");
+            this.name = name;
+
+            List<String> r1 = Transport.read("!common_params.txt");
+
+            this.keys = new CryptoSystem.Keys(new BigInteger(r.get(2)),
+                    new BigInteger(r.get(4)),
+                    new BigInteger(r1.get(1)));
+        }
+
+        static CryptoSystem.Keys genKeys(BigInteger p) throws NoSuchAlgorithmException {
+            return new CryptoSystem.Keys(p);
+        }
+
+        private StringBuilder encrypt(List<String> cards) {
             StringBuilder encryptCards = new StringBuilder();
             cards.forEach(card -> {
-                if (isCardsStr) {
-                    encryptCards.append(CryptoSystem.encryptString(card, this.keys)).append("\n");
-                } else {
-                    encryptCards.append(CryptoSystem.encrypt(card, this.keys)).append("\n");
-                }
+                encryptCards.append(CryptoSystem.encrypt(card, this.keys)).append("\n");
             });
             return encryptCards;
         }
 
-        private StringBuilder decrypt(List<String> cryptCards, Boolean isCardsStr) {
+        private StringBuilder decrypt(List<String> cryptCards) {
             StringBuilder decryptCards = new StringBuilder();
             cryptCards.forEach(card -> {
-                if (isCardsStr) {
-                    decryptCards.append(CryptoSystem.decryptString(card, this.keys)).append("\n");
-                } else {
-                    decryptCards.append(CryptoSystem.decrypt(card, this.keys)).append("\n");
-                }
+                decryptCards.append(CryptoSystem.decrypt(card, this.keys)).append("\n");
             });
             return decryptCards;
         }
 
-        private int[] getFiveRandIndexes(int n) {
+        //Выбрать amount рандомных индексов
+        private int[] getRandIndexes(int amount, int n) throws Exception {
+            if (amount > n) {
+                throw new Exception("Карты закончились");
+            }
+
             boolean[] used = new boolean[n];
             for (int i = 0; i < n; i++) {
                 used[i] = false;
             }
 
-            int[] indexes = new int[5];
+            int[] indexes = new int[amount];
             Random random = new Random();
-            for (int i = 0; i < 5; i++) {
+            for (int i = 0; i < amount; i++) {
                 int r = random.nextInt(n);
                 while (used[r]) {
                     r = random.nextInt(n);
@@ -65,7 +77,8 @@ public class Protocol {
             return indexes;
         }
 
-        private List<String> getFiveCards(List<String> cards, int[] indexes) {
+        //Выбрать карты по индексам
+        private List<String> getCardsByIndexes(List<String> cards, int[] indexes) {
             List<String> fiveCards = new ArrayList<>();
             for (int index : indexes) {
                 fiveCards.add(cards.get(index));
@@ -73,11 +86,12 @@ public class Protocol {
             return fiveCards;
         }
 
-        private List<String> getCardsForNextUser() {
+        //Новые карты
+        private List<String> getNewCards(List<String> cards, int[] indexes) {
             List<String> nextCards = new ArrayList<>();
-            for (int i = 0; i < this.cards.size(); i++) {
+            for (int i = 0; i < cards.size(); i++) {
                 boolean ok = true;
-                for (int j = 0; j < this.indexes.length; j++) {
+                for (int j = 0; j < indexes.length; j++) {
                     if (i == j) {
                         ok = false;
                         break;
@@ -98,53 +112,35 @@ public class Protocol {
             return str;
         }
 
-        List<String> cards = null;
-        int[] indexes = null;
+        void selectCards(String path, String outPath, int amountCard, boolean withCrypt) throws Exception {
+            List<String> cards = Transport.read(path);
+            int[] indexes = getRandIndexes(amountCard, cards.size());
+            List<String> c = getCardsByIndexes(cards, indexes);
 
-        void selectionOfFiveCards(String path, String outPath) {
-            this.cards = Transport.read(path, true);
-            this.indexes = getFiveRandIndexes(this.cards.size());
-            List<String> fiveCards = getFiveCards(this.cards, this.indexes);
-            StringBuilder out = this.encrypt(fiveCards, false);
-            Transport.write(this.name + "-" + outPath, String.valueOf(out));
+            StringBuilder out;
+            if (withCrypt) {
+                out = this.encrypt(c);
+            } else {
+                out = listToStringBuilder(c);
+            }
+            Transport.write(outPath, String.valueOf(out));
+
+            List<String> newCards = getNewCards(cards, indexes);
+            out = listToStringBuilder(newCards);
+
+            Transport.write("cards.txt", String.valueOf(out));
         }
 
-        void selectionOfFiveCardsForHead(String path, String outPath) {
-            List<String> cards = Transport.read(path, true);
-            int[] randIndexes = getFiveRandIndexes(cards.size());
-            List<String> fiveCards = getFiveCards(cards, randIndexes);
-            StringBuilder out = this.listToStringBuilder(fiveCards);
-            Transport.write(this.name + "-" + outPath, String.valueOf(out));
-        }
-
-        void sendCardsForNextUser(String outPath) {
-            List<String> cards = getCardsForNextUser();
-            Collections.shuffle(cards);
-            StringBuilder out = listToStringBuilder(cards);
-            Transport.write(this.name + "-" + outPath, String.valueOf(out));
-
-            this.cards = null;
-            this.indexes = null;
+        void encryptCards(String path, String outPath) {
+            List<String> cards = Transport.read(path);
+            StringBuilder out = this.encrypt(cards);
+            Transport.write(outPath, String.valueOf(out));
         }
 
         void decryptCards(String path, String outPath) {
-            List<String> cards = Transport.read(path, true);
-            StringBuilder userOut = this.decrypt(cards, true);
-            Transport.write(this.name + "-" + outPath, String.valueOf(userOut));
-        }
-
-        //For head
-        void sendCardsForNextUserHead(String outPath) {
-            List<String> cards = Arrays.asList(Cards.getCards());
-            Collections.shuffle(cards);
-            StringBuilder out = this.encrypt(cards, true);
-            Transport.write(this.name + "-" + outPath, String.valueOf(out));
-        }
-
-        void decryptCardsHead(String path, String outPath) {
-            List<String> cards = Transport.read(path, true);
-            StringBuilder userOut = this.decrypt(cards, false);
-            Transport.write(this.name + "-" + outPath, String.valueOf(userOut));
+            List<String> cards = Transport.read(path);
+            StringBuilder userOut = this.decrypt(cards);
+            Transport.write(outPath, String.valueOf(userOut));
         }
 
         @Override
@@ -164,6 +160,12 @@ public class Protocol {
                 genKeys(p);
             }
 
+            Keys(BigInteger c, BigInteger d, BigInteger p) {
+                this.c = c;
+                this.d = d;
+                this.p = p;
+            }
+
             private void genKeys(BigInteger p) throws NoSuchAlgorithmException {
                 //Генерация с взаимно простого с p - 1
                 this.c = CryptoSystem.getRandBigInteger();
@@ -177,29 +179,24 @@ public class Protocol {
 
             @Override
             public String toString() {
-                return "c = " + this.c + "\nd = " + this.d + " ";
+                return "c\n" + this.c + "\nd\n" + this.d;
             }
         }
 
         //Шифровать
-        static BigInteger encryptString(String message, Keys keys) {
+        static String encrypt(String message, Keys keys) {
             byte[] byteMessage = message.getBytes();
-            return new BigInteger(byteMessage).modPow(keys.c, keys.p);
-        }
-
-        static BigInteger encrypt(String message, Keys keys) {
-            return new BigInteger(message).modPow(keys.c, keys.p);
-        }
-
-        //Дешифроавть
-        static String decryptString(String message, Keys keys) {
-            BigInteger res = CryptoSystem.decrypt(message, keys);
-            byte[] byteRes = res.toByteArray();
+            BigInteger encrypt = new BigInteger(byteMessage).modPow(keys.c, keys.p);
+            byte[] byteRes = encrypt.toByteArray();
             return new String(byteRes);
         }
 
-        static BigInteger decrypt(String message, Keys keys) {
-            return new BigInteger(message).modPow(keys.d, keys.p);
+        //Дешифроавть
+        static String decrypt(String message, Keys keys) {
+            byte[] byteMessage = message.getBytes();
+            BigInteger decrypt = new BigInteger(byteMessage).modPow(keys.d, keys.p);
+            byte[] byteRes = decrypt.toByteArray();
+            return new String(byteRes);
         }
 
         BigInteger genPrimeNum() throws NoSuchAlgorithmException {
@@ -234,11 +231,9 @@ public class Protocol {
     static class Transport {
         private static final String pathPrefix = "./Protocols/protocolWork/";
 
-        public static List<String> read(String file, boolean withPrefix) {
-            String prefix = withPrefix ? pathPrefix : "";
-
+        public static List<String> read(String file) {
             List<String> lines = new ArrayList<>();
-            try (Stream<String> stream = Files.lines(Paths.get(prefix + file))) {
+            try (Stream<String> stream = Files.lines(Paths.get(pathPrefix + file))) {
                 lines = stream.collect(Collectors.toList());
             } catch (IOException e) {
                 e.printStackTrace();
@@ -262,92 +257,105 @@ public class Protocol {
         }
     }
 
-    void init() throws NoSuchAlgorithmException {
+    void init() throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Введите имя участника:");
+        String name = sc.nextLine();
+
+        System.out.println("Что вы хотите сделать?");
+        System.out.println("1 - Сгенерировать простое p");
+        System.out.println("2 - Сгенерировать параметры системы (c, d)");
+        System.out.println("3 - Сгенерировать новую колоду карт");
+
+        System.out.println("4 - Выбрать карты (с/без шифрование)");
+
+        System.out.println("5 - Шифрование карт");
+        System.out.println("6 - Дешифрование карт");
+        int action = sc.nextInt();
+
+        switch (action) {
+            case 1: {
+                genPrimal();
+                break;
+            }
+            case 2: {
+                genParams(name);
+                break;
+            }
+            case 3: {
+                genCards();
+                break;
+            }
+            case 4: {
+                User user = new User(name);
+                System.out.println("Введите количество выбираемых карт:"); // sc.nextLine(); //??
+                int amountCard = sc.nextInt();
+                System.out.println("Карты нужно шифровать? (да - 1, нет - 2)"); // sc.nextLine(); //??
+                boolean withCrypt = sc.nextInt() == 1;
+
+                System.out.println("Введите имя файла, в котором хранятся карты"); sc.nextLine();
+                String path = sc.nextLine();
+                System.out.println("Введите имя файла, в котором будет храниться результат действий");
+                String outPath = sc.nextLine();
+
+                user.selectCards(path, outPath, amountCard, withCrypt);
+                break;
+            }
+            case 5: {
+                System.out.println("Введите имя файла, в котором хранятся карты"); sc.nextLine();
+                String path = sc.nextLine();
+                System.out.println("Введите имя файла, в котором будет храниться результат действий");
+                String outPath = sc.nextLine();
+
+                User user = new User(name);
+                user.encryptCards(path, outPath);
+                break;
+            }
+            case 6: {
+                System.out.println("Введите имя файла, в котором хранятся карты"); sc.nextLine();
+                String path = sc.nextLine();
+                System.out.println("Введите имя файла, в котором будет храниться результат действий");
+                String outPath = sc.nextLine();
+
+                User user = new User(name);
+                user.decryptCards(path, outPath);
+                break;
+            }
+        }
+    }
+
+    void genPrimal() throws NoSuchAlgorithmException {
+        //Генерация простого p
         BigInteger p = new CryptoSystem().genPrimeNum();
 
-        List<User> users = new ArrayList<>();
-        List<String> usersName = Transport.read("input.txt", false);
-        usersName.forEach(name -> {
-            try {
-                users.add(new User(name, p));
-            } catch (NoSuchAlgorithmException e) {
-                e.printStackTrace();
-            }
-        });
-
         StringBuilder out = new StringBuilder();
-        out.append("p = ").append(p).append('\n');
-        users.forEach(user -> {
-            out.append(user.name).append('\n').append(user.keys.toString()).append('\n');
+        out.append("p\n").append(p).append('\n');
+        Transport.write("!common_params.txt", String.valueOf(out));
+    }
+
+    void genParams(String user) {
+        List<String> pStr = Transport.read("!common_params.txt");
+        BigInteger p = new BigInteger(pStr.get(1));
+
+        StringBuilder outTemp = new StringBuilder();
+        try {
+            outTemp.append(user).append('\n').append(User.genKeys(p).toString()).append('\n');
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        Transport.write("!" + user + "_params.txt", String.valueOf(outTemp));
+    }
+
+    void genCards() {
+        Random random = new Random();
+
+        List<String> cards = Arrays.asList(Cards.getCards());
+        //Вывод карт
+        StringBuilder out = new StringBuilder();
+        cards.forEach(card -> {
+            out.append(card).append(random.nextInt(10000000)).append('\n');
         });
-        Transport.write("!user_params.txt", String.valueOf(out));
-
-        if (users.size() > 1 && users.size() < 6) {
-            this.run(users);
-        } else {
-            System.out.println("У вас введено не корректное число участников.");
-        }
-    }
-
-    void continueExecution(Scanner scanner) {
-        //System.out.println("Продолжить? (enter)");
-        //scanner.nextLine();
-    }
-
-    void run(List<User> users) {
-        String CARDS = "cards.txt", FIVE_CARDS = "five_cards.txt", FIVE_CARDS_HEAD = "five_cards_head.txt", FIVE_CARDS_END = "five_cards_end.txt";
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("---------------------");
-        System.out.println("Раздача началась");
-
-        User headUser = users.get(0);
-
-        //Head
-        System.out.println("Организатор игры взял колоду, зашифровал, передал");
-        headUser.sendCardsForNextUserHead(CARDS);
-
-        continueExecution(scanner);
-
-        //Other users
-        System.out.println("Участники выбирают себе карты (5 шт), шифруют их, и отправляют организатору");
-        for (int i = 1; i < users.size(); i++) {
-            User user = users.get(i);
-
-            user.selectionOfFiveCards(users.get(i - 1).name + "-" + CARDS, FIVE_CARDS);
-            user.sendCardsForNextUser(CARDS);
-
-            continueExecution(scanner);
-
-            if (i == users.size() - 1) {
-                System.out.println("Последний участник выбирает карты организатору (5 шт)");
-
-                user.selectionOfFiveCardsForHead(user.name + "-" + CARDS, FIVE_CARDS_HEAD);
-                continueExecution(scanner);
-            }
-        }
-
-        //Head do this
-        System.out.println("Организатор дешифрует полученные карты и отправляет участникам");
-        for (int i = 0; i < users.size(); i++) {
-            if (i == 0) {
-                System.out.println("Организатор дешифрует свои карты");
-                headUser.decryptCards(users.get(users.size() - 1).name + "-" + FIVE_CARDS_HEAD, FIVE_CARDS_END);
-            } else {
-                headUser.decryptCardsHead(users.get(i).name + "-" + FIVE_CARDS, "for-" + users.get(i).name + "-" + FIVE_CARDS);
-            }
-            continueExecution(scanner);
-        }
-
-        //Other
-        System.out.println("Участники дешифруют свои карты");
-        for (int i = 1; i < users.size(); i++) {
-            User user = users.get(i);
-            user.decryptCards(headUser.name + "-for-" + users.get(i).name + "-" + FIVE_CARDS, FIVE_CARDS_END);
-            continueExecution(scanner);
-        }
-
-        System.out.println("Конец раздачи");
+        Transport.write("!start_cards.txt", String.valueOf(out));
     }
 }
