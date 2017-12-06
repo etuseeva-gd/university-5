7bb0b74fa905e7bf7c36aea34dd0d2769ba1b13a
lena <lenok-cotik@yandex.ru>
2017-12-06 03:30:39 +0300

    ...

--- NeuronNetworks/script.js
+++ NeuronNetworks/script.js
@@ -3,17 +3,294 @@ const fs = require('fs');
 
 class Graph {
     //Граф подается как из файла
-    constructor(data) {
-        const graphObj = parseGraphList(data);
+    constructor(data, isBnf = false) {
+        this.graphArray = null;
+        this.graph = null;
+        this.graphBnf = null;
 
-        this.graph = {};
-        for (let i = 0; i < graphObj.length; i++) {
-            this.graph[graphObj[i][0]] = graphObj[i][1].filter(v => v !== '');
+        if (data) {
+            if (!isBnf) {
+                // [ [ 'a', [ 'b' ] ], [ 'b', [ 'c' ] ], [ 'c', [ 'a' ] ] ]
+                this.graphArray = this.parseGraphList(data);
+                // { a: [ 'b' ], b: [ 'c' ], c: [ 'a' ] } }
+                this.graph = this.createGraph(this.graphArray);
+            } else {
+                this.graphBnf = this.parseBnfGraphToEdges(data);
+            }
         }
     }
 
-    createGraph() {
+    getBnf(graph) {
+        const graphArray = graph ? graph : this.graphArray;
 
+        const bnf = {};
+        const usedVertexes = [];
+
+        graphArray.forEach(obj => {
+            const [vertex, vertexes] = obj;
+            bnf[vertex] = [];
+            vertexes.forEach(v => {
+                if (v !== '') {
+                    let num;
+                    if (!usedVertexes[v]) {
+                        usedVertexes[v] = 1;
+                        num = 1;
+                    } else {
+                        num = ++usedVertexes[v];
+                    }
+                    bnf[vertex].push(`${num}-${v}`);
+                } else {
+                    bnf[vertex].push('_');
+                }
+            });
+        });
+
+        let answer = '';
+        for (const vertex in bnf) {
+            answer += `${vertex} : `;
+
+            const bnfVer = bnf[vertex];
+            bnfVer.forEach((v, i) => {
+                const postfix = i === bnfVer.length - 1 ? '' : ', ';
+                answer += `${v}${postfix}`;
+            });
+            answer += '\n';
+        }
+
+        return answer;
+    }
+
+    getFunctionFromBnf() {
+        let answer = this.getFunctionFromBNF(this.graphBnf.sinks, this.graphBnf.edges);
+        answer = JSON.stringify(answer);
+
+        const signs = ['"', ':', '{', '}'],
+            newSigns = ['', '', '(', ')'];
+
+        signs.forEach((s, i) => {
+            answer = answer.split(s).join(newSigns[i]);
+        });
+
+        return answer;
+    }
+
+    calcFunctionFromGraph(operationsFileName) {
+        const bnf = this.getBnf();
+        const parseBnf = this.parseBnfGraphToEdges(bnf);
+        const funcObj = this.getFunctionFromBNF(parseBnf.sinks, parseBnf.edges);
+
+        let dataOperations = readFile(operationsFileName);
+        dataOperations = dataOperations.split('\r').join('').split(' ').join('');
+
+        const operations = {};
+        const lines = dataOperations.split('\n');
+        lines.forEach(line => {
+            const parse = line.split(':');
+            if (parse[1] !== '') {
+                operations[parse[0]] = parse[1];
+            }
+        });
+
+        const result = this.calcResultForFunction(funcObj, operations);
+
+        let answer = '';
+        result.forEach(r => {
+            answer += `Для ${r.vertex} = ${r.value}\n`;
+        });
+
+        return answer;
+    }
+
+    //Вход: данные из файла
+    getBnfFromFunction(data) {
+        let signs = ['(', '(', ')'],
+            newSigns = [':(', '{', '}'];
+
+        signs.forEach((s, i) => {
+            data = data.split(s).join(newSigns[i]);
+        });
+        data = data.substr(1);
+
+        signs = ['{', '}', ':', ','];
+
+        let dataCopy = '';
+        for (let i = 0; i < data.length; i++) {
+            if (signs.find(s => s === data[i])) {
+                dataCopy += data[i];
+            } else {
+                let j = i + 1;
+                while (!signs.find(s => s === data[j])) {
+                    j++;
+                }
+                dataCopy += `"${data.substring(i, j)}"`;
+                i = j - 1;
+            }
+        }
+
+        //Todo: проверка на корректность
+
+        const funcObj = JSON.parse(dataCopy);
+
+        const listEdges = {};
+        this.fromFuncToGraph(funcObj, listEdges);
+
+        const objGraph = [];
+        for (const v in listEdges) {
+            if (listEdges[v].length === 0) {
+                listEdges[v].push('');
+            }
+            objGraph.push([v, listEdges[v]]);
+        }
+
+        return this.getBnf(objGraph);
+    }
+
+    //Вход: данные из файла
+    parseGraphList(data) {
+        data = data.split('\r').join('').split(' ').join('');
+
+        const lines = data.split('\n');
+
+        const objGraph = [];
+        lines.forEach(line => {
+            const [vertex, vertexesStr] = line.split(':');
+            const vertexes = vertexesStr.split(',');
+            objGraph.push([vertex, vertexes]);
+        });
+
+        return objGraph;
+    }
+
+    //Вход: данные из файла
+    parseBnfGraphToEdges(data) {
+        data = data.split('\r').join('').split(' ').join('');
+
+        const graphEdges = [];
+        const sinks = [];
+
+        const lines = data.split('\n');
+        lines.forEach(line => {
+            if (line !== '') {
+                const [vertex, vertexesStr] = line.split(':');
+                const vertexes = vertexesStr.split(',');
+                vertexes.forEach(v => {
+                    if (v === '_') {
+                        sinks.push(vertex);
+                    } else {
+                        graphEdges.push([vertex, v.split('-')[1]]);
+                    }
+                });
+            }
+        });
+
+        return {sinks, edges: graphEdges};
+    }
+
+    //Вспомогательная функция
+    getFunctionFromBNF(sinks, edges) {
+        const result = {};
+        sinks.forEach(s => {
+            const tmpEdges = edges.slice(0);
+            result[s] = this.getVertexesForSink(s, tmpEdges);
+        });
+        return result;
+    }
+
+    //Вспомогательная функция
+    getVertexesForSink(sink, edges) {
+        const result = {};
+        for (let i = 0; i < edges.length; i++) {
+            if (edges[i][1] === sink) {
+                let vertex = edges[i][0];
+                edges.splice(i--, 1);
+                result[vertex] = this.getVertexesForSink(vertex, edges.slice(0));
+            }
+        }
+        return result;
+    }
+
+    //Вспомогательная функция
+    calcResultForFunction(obj, operations) {
+        const result = [];
+        for (let o in obj) {
+            const numbers = this.calcNumbersForVertexes(obj[o], operations);
+            const res = this.calcResultByOperation(operations[o], numbers);
+            result.push({vertex: o, value: res});
+        }
+        return result;
+    }
+
+    //Вспомогательная функция
+    calcResultByOperation(operation, numbers) {
+        switch (operation) {
+            case '+': {
+                return numbers[0] + numbers[1];
+            }
+            case '*': {
+                return numbers[0] * numbers[1];
+            }
+            case '&': {
+                return numbers[0] & numbers[1];
+            }
+            case '|': {
+                return numbers[0] | numbers[1];
+            }
+            case '>>': {
+                return numbers[0] >> numbers[1];
+            }
+            case '<<': {
+                return numbers[0] << numbers[1];
+            }
+            case '!': {
+                return ~numbers[0];
+            }
+        }
+    }
+
+    //Вспомогательная функция
+    calcNumbersForVertexes(obj, operations) {
+        let result = [];
+        for (let o in obj) {
+            if (o >= '0' && o <= '9') {
+                result.push(+o);
+            } else {
+                const numbers = this.calcNumbersForVertexes(obj[o], operations);
+                const res = this.calcResultByOperation(operations[o], numbers);
+                result.push(res);
+            }
+        }
+        return result;
+    }
+
+    //Вспомогательная функция
+    fromFuncToGraph(funcObj, answer) {
+        for (const vertex in funcObj) {
+            if (!answer[vertex]) {
+                answer[vertex] = [];
+            }
+
+            if (funcObj[vertex] !== {}) {
+                const parents = [];
+                for (const v in funcObj[vertex]) {
+                    parents.push(v);
+                }
+                parents.forEach(p => {
+                    if (!answer[p]) {
+                        answer[p] = [];
+                    }
+                    answer[p].push(vertex);
+                });
+            }
+            this.fromFuncToGraph(funcObj[vertex], answer);
+        }
+    }
+
+    createGraph(graphArray) {
+        const graph = {};
+        for (let i = 0; i < graphArray.length; i++) {
+            graph[graphArray[i][0]] = graphArray[i][1].filter(v => v !== '');
+        }
+        return graph;
     }
 
     //Если ацикличный, то вернет true
@@ -30,6 +307,7 @@ class Graph {
         return !this.cycle;
     }
 
+    //Обход в глубину
     dfs(v, color, p) {
         color[v] = 1;
         for (let i = 0; i < this.graph[v].length; i++) {
@@ -67,330 +345,41 @@ function writeFile(fileName, text) {
 //     writeFile(`xml_${name}.txt`, xml);
 // }
 
-function parseGraphList(data) {
-    data = data.split('\r').join('').split(' ').join('');
-
-    const lines = data.split('\n');
-
-    const objGraph = [];
-    lines.forEach(line => {
-        const [vertex, vertexesStr] = line.split(':');
-        const vertexes = vertexesStr.split(',');
-        objGraph.push([vertex, vertexes]);
-    });
-
-    return objGraph;
-}
-
-function parseBnfGraphToEdges(data) {
-    data = data.split('\r').join('').split(' ').join('');
-
-    const graphEdges = [];
-    const sinks = [];
-
-    const lines = data.split('\n');
-    lines.forEach(line => {
-        if (line !== '') {
-            const [vertex, vertexesStr] = line.split(':');
-            const vertexes = vertexesStr.split(',');
-            vertexes.forEach(v => {
-                if (v === '_') {
-                    sinks.push(vertex);
-                } else {
-                    graphEdges.push([vertex, v.split('-')[1]]);
-                }
-            });
-        }
-    });
-
-    return {sinks, edges: graphEdges};
-}
-
-function getFunctionFromBNF(sinks, edges) {
-    const result = {};
-    sinks.forEach(s => {
-        const tmpEdges = edges.slice(0);
-        result[s] = getVertexesForSink(s, tmpEdges);
-    });
-    return result;
-}
-
-function getVertexesForSink(sink, edges) {
-    const result = {};
-    for (let i = 0; i < edges.length; i++) {
-        if (edges[i][1] === sink) {
-            let vertex = edges[i][0];
-            edges.splice(i--, 1);
-            result[vertex] = getVertexesForSink(vertex, edges.slice(0));
-        }
-    }
-    return result;
-}
-
-//Входные данные - список списков, выход - БНФ графа
-function first(data) {
-    //Todo: проверки валидности
-    return fromObjToBNF(parseGraphList(data));
-}
-
-function fromObjToBNF(objGraph) {
-    const bnf = {};
-    const usedVertexes = [];
-
-    objGraph.forEach(obj => {
-        const [vertex, vertexes] = obj;
-        bnf[vertex] = [];
-        vertexes.forEach(v => {
-            if (v !== '') {
-                let num;
-                if (!usedVertexes[v]) {
-                    usedVertexes[v] = 1;
-                    num = 1;
-                } else {
-                    num = ++usedVertexes[v];
-                }
-                bnf[vertex].push(`${num}-${v}`);
-            } else {
-                bnf[vertex].push('_');
-            }
-        });
-    });
-
-    let answer = '';
-    for (const vertex in bnf) {
-        answer += `${vertex} : `;
-
-        const bnfVer = bnf[vertex];
-        bnfVer.forEach((v, i) => {
-            const postfix = i === bnfVer.length - 1 ? '' : ', ';
-            answer += `${v}${postfix}`;
-        });
-        answer += '\n';
-    }
-
-    return answer;
-}
-
-//Входные данные - БНФ графа, Выход - функция
-function second(data) {
-    //Todo: проверки валидности
-    //Todo: проверка на циклы
-
-    const parseBnf = parseBnfGraphToEdges(data);
-    let answer = getFunctionFromBNF(parseBnf.sinks, parseBnf.edges);
-
-    answer = JSON.stringify(answer);
-
-    const signs = ['"', ':', '{', '}'],
-        newSigns = ['', '', '(', ')'];
-
-    signs.forEach((s, i) => {
-        answer = answer.split(s).join(newSigns[i]);
-    });
-
-    return answer;
-}
-
-function third() {
-    const dataGraph = readFile('input1.txt');
-
-    const bnf = first(dataGraph);
-    const parseBnf = parseBnfGraphToEdges(bnf);
-    const funcObj = getFunctionFromBNF(parseBnf.sinks, parseBnf.edges);
-
-    console.log(JSON.stringify(funcObj));
-
-    let dataOperations = readFile('input2.txt');
-    dataOperations = dataOperations.split('\r').join('').split(' ').join('');
-
-    const operations = {};
-    const lines = dataOperations.split('\n');
-    lines.forEach(line => {
-        const parse = line.split(':');
-        if (parse[1] !== '') {
-            operations[parse[0]] = parse[1];
-        }
-    });
-    console.log(operations);
-
-    const result = calcResultForFunction(funcObj, operations);
-
-    let answer = '';
-    result.forEach(r => {
-        answer += `Для ${r.vertex} = ${r.value}\n`;
-    });
-
-    writeFile('output.txt', answer);
-}
-
-function calcResultForFunction(obj, operations) {
-    const result = [];
-    for (let o in obj) {
-        const numbers = calcNumbersForVertexes(obj[o], operations);
-        console.log(numbers);
-        const res = calcResultByOperation(operations[o], numbers);
-        console.log(res);
-        result.push({vertex: o, value: res});
-    }
-    return result;
-}
-
-function calcResultByOperation(operation, numbers) {
-    switch (operation) {
-        case '+': {
-            return numbers[0] + numbers[1];
-        }
-        case '*': {
-            return numbers[0] * numbers[1];
-        }
-        case '&': {
-            return numbers[0] & numbers[1];
-        }
-        case '|': {
-            return numbers[0] | numbers[1];
-        }
-        case '>>': {
-            return numbers[0] >> numbers[1];
-        }
-        case '<<': {
-            return numbers[0] << numbers[1];
-        }
-        case '!': {
-            return ~numbers[0];
-        }
-    }
-}
-
-function calcNumbersForVertexes(obj, operations) {
-    let result = [];
-    for (let o in obj) {
-        if (o >= '0' && o <= '9') {
-            result.push(+o);
-        } else {
-            const numbers = calcNumbersForVertexes(obj[o], operations);
-            const res = calcResultByOperation(operations[o], numbers);
-            result.push(res);
-        }
-    }
-    return result;
-}
-
-//Выходные данные - функция графа, Выход - БНФ графа
-function fourth(data) {
-    let signs = ['(', '(', ')'],
-        newSigns = [':(', '{', '}'];
-
-    signs.forEach((s, i) => {
-        data = data.split(s).join(newSigns[i]);
-    });
-    data = data.substr(1);
-
-    signs = ['{', '}', ':', ','];
-
-    let dataCopy = '';
-    for (let i = 0; i < data.length; i++) {
-        if (signs.find(s => s === data[i])) {
-            dataCopy += data[i];
-        } else {
-            let j = i + 1;
-            while (!signs.find(s => s === data[j])) {
-                j++;
-            }
-            dataCopy += `"${data.substring(i, j)}"`;
-            i = j - 1;
-        }
-    }
-
-    // console.log(data);
-    // console.log(dataCopy);
-
-    //Todo: проверка на корректность
-    const funcObj = JSON.parse(dataCopy);
-    console.log(funcObj);
-
-    const listEdges = {};
-    fromFuncToGraph(funcObj, listEdges);
-    console.log(listEdges);
-
-    const objGraph = [];
-    for (const v in listEdges) {
-        if (listEdges[v].length === 0) {
-            listEdges[v].push('');
-        }
-        objGraph.push([v, listEdges[v]]);
-    }
-
-    return fromObjToBNF(objGraph);
-}
-
-function fromFuncToGraph(funcObj, answer) {
-    for (const vertex in funcObj) {
-        if (!answer[vertex]) {
-            answer[vertex] = [];
-        }
-
-        if (funcObj[vertex] !== {}) {
-            const parents = [];
-            for (const v in funcObj[vertex]) {
-                parents.push(v);
-            }
-            parents.forEach(p => {
-                if (!answer[p]) {
-                    answer[p] = [];
-                }
-                answer[p].push(vertex);
-            });
-        }
-        fromFuncToGraph(funcObj[vertex], answer);
-    }
-}
-
 //Main
 
 console.log('Введите номер задания:');
-console.log('1: Создание графа');
-console.log('2: Создание функции по графу');
-console.log('3: Вычисление значения функции на графе');
-console.log('4: Построение графа по функции, переданной в префиксной скобочной записи');
-
+console.log('1: Создание графа (Вход: Список списков графа)');
+console.log('2: Создание функции по графу (Вход: БНФ графа)');
+console.log('3: Вычисление значения функции на графе (Вход: Список списков графа, операции)');
+console.log('4: Построение графа по функции, переданной в префиксной скобочной записи (Вход: Функция графа)');
 // console.log('5: Построение многослойной нейронной сети');
 // console.log('6: Реализация метода обратного распространения ошибки для многослойной НС');
-
-console.log('0: Выход');
+console.log('P.S. Входной файл: input.txt, Выходной файл: output.txt, Файл с операциями для 3 задания в operations.txt')
 
 const stdin = process.openStdin();
 stdin.addListener('data', (data) => {
     const fileInput = 'input.txt', fileOutput = 'output.txt';
-
-    let data1 = readFile(fileInput);
-    // writeFile(fileOutput, first(data));
-    // writeFile(fileOutput, second(data));
-    // writeFile(fileOutput, fourth(data));
-
-    // third();
-
-    const g = new Graph(data1);
-
+    const fileData = readFile(fileInput);
     const action = data.toString().trim();
     switch (action) {
-        case '0': {
-            process.exit(0);
-            break;
-        }
         case '1': {
-            first();
+            const graph = new Graph(fileData);
+            writeFile(fileOutput, graph.getBnf());
             break;
         }
         case '2': {
-            second();
+            const graph = new Graph(fileData, true);
+            writeFile(fileOutput, graph.getFunctionFromBnf());
             break;
         }
         case '3': {
-            third();
+            const graph = new Graph(fileData);
+            writeFile(fileOutput, graph.calcFunctionFromGraph('operations.txt'));
             break;
         }
         case '4': {
-            fourth();
+            const graph = new Graph();
+            writeFile(fileOutput, graph.getBnfFromFunction(fileData));
             break;
         }
         case '5': {
@@ -403,6 +392,5 @@ stdin.addListener('data', (data) => {
             console.log('Некорректные данные!');
         }
     }
-
-    console.log("Введите следующее действие:");
+    process.exit(0);
 });
