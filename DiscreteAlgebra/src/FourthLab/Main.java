18b9722e2990e56dac69a60c00a67d1d0f839549
lena <lenok-cotik@yandex.ru>
2017-12-10 22:52:00 +0300

    ...

--- DiscreteAlgebra/src/FourthLab/Main.java
+++ DiscreteAlgebra/src/FourthLab/Main.java
@@ -210,268 +210,6 @@ public class Main {
 
     }
 
-    int[] mod(int[] x, int[] y, int p) {
-        ArrayList<Integer> z = new ArrayList<>();
-        for (int aX : x) {
-            z.add(aX);
-        }
-        if (x.length < y.length || extendedEuclid(y[y.length - 1], p) != 1) {
-            int[] result = new int[z.size()];
-            for (int i = 0; i < result.length; i++) {
-                result[i] = z.get(i);
-            }
-            return result;
-        }
-        int degX = x.length - 1;
-        int degY = y.length - 1;
-        int inverse = modInverse(y[y.length - 1], p);
-        while (z.size() >= y.length) {
-            int degZ = degX - degY;
-            int element = (inverse * z.get(degX)) % p;
-            for (int i = 0; i < degY + 1; i++) {
-                z.set(i + degZ, (z.get(i + degZ) - y[i] * element) % p);
-                if (z.get(i + degZ) < 0) {
-                    z.set(i + degZ, z.get(i + degZ) + p);
-                }
-            }
-            while (z.size() > 1 && z.get(z.size() - 1) == 0) {
-                z.remove(z.size() - 1);
-            }
-            degX = z.size() - 1;
-            if (z.get(z.size() - 1) == 0) {
-                break;
-            }
-        }
-        while (z.size() > 1 && z.get(z.size() - 1) == 0) {
-            z.remove(z.size() - 1);
-        }
-        int[] result = new int[z.size()];
-        for (int i = 0; i < result.length; i++) {
-            result[i] = z.get(i);
-        }
-        return result;
-    }
-
-    int modInverse(int x, int p) {
-        return (extendedEuclid(x, p) % p + p) % p;
-    }
-
-    int extendedEuclid(int a, int b) {
-        int x, y;
-        int r0 = a, r1 = b, x0 = 1, x1 = 0, y0 = 0, y1 = 1;
-        while (true) {
-            int q = r0 / r1;
-            int r = r0 % r1;
-            if (r == 0)
-                break;
-            else {
-                r0 = r1;
-                r1 = r;
-                x = x0 - q * x1;
-                x0 = x1;
-                x1 = x;
-                y = y0 - q * y1;
-                y0 = y1;
-                y1 = y;
-            }
-        }
-        x = x1;
-        y = y1;
-        return x;
-    }
-
-    int[] add(int[] x, int[] y, int p) {
-        List<Integer> z = new ArrayList<>();
-        if (x.length > y.length) {
-            for (int aX : x) {
-                z.add(aX);
-            }
-        } else {
-            for (int aY : y) {
-                z.add(aY);
-            }
-        }
-        for (int i = 0; i < z.size(); i++) {
-            z.set(i, (x[i] + y[i]) % p);
-        }
-        while (z.size() > 1 && z.get(z.size() - 1) == 0) {
-            z.remove(z.size() - 1);
-        }
-        int[] result = new int[z.size()];
-        for (int i = 0; i < result.length; i++) {
-            result[i] = z.get(i);
-        }
-        return result;
-    }
-
-    int[] mult(int[] x, int[] y, int p) {
-        ArrayList<Integer> z = new ArrayList<>();
-        for (int i = 0; i < x.length + y.length; i++) {
-            z.add(0);
-        }
-        for (int i = 0; i < x.length; i++) {
-            for (int j = 0; j < y.length; j++) {
-                z.set(i + j, (z.get(i + j) + ((x[i] * y[j]) % p)) % p);
-            }
-        }
-        while (z.size() > 1 && z.get(z.size() - 1) == 0) {
-            z.remove(z.size() - 1);
-        }
-
-        int[] result = new int[z.size()];
-        for (int i = 0; i < result.length; i++) {
-            result[i] = z.get(i);
-        }
-        return result;
-    }
-
-    List<int[]> getElementsExtention(int p, int[] m) {
-        int amount = (int) Math.pow(p, m.length - 1);
-        List<int[]> expansion = new ArrayList<>();
-        int[] polynomial = new int[m.length];
-        for (int i = 0; i < amount; i++) {
-            expansion.add(polynomial);
-            polynomial = getPolynomial(polynomial, p);
-        }
-        return expansion;
-    }
-
-    int[] getPolynomial(int[] m, int p) {
-        int[] polynomial = m.clone();
-        for (int i = 0; i < polynomial.length; i++) {
-            if (polynomial[i] == p - 1) {
-                polynomial[i] = 0;
-            } else {
-                polynomial[i]++;
-                break;
-            }
-        }
-        return polynomial;
-    }
-
-    String printPolynomial(int[] m) {
-        StringBuilder builder = new StringBuilder();
-        boolean flag = true;
-        for (int i = 0; i < m.length; i++) {
-            if (m[i] != 0) {
-                if (i > 0 && !flag) {
-                    builder.append("+");
-                }
-                flag = false;
-                if (i == 0) {
-                    builder.append(m[i]);
-                } else {
-                    if (i == 1) {
-                        if (m[i] > 1) {
-                            builder.append(m[i]);
-                            builder.append("*");
-                        }
-
-                        builder.append("x");
-                    } else {
-                        if (m[i] > 1) {
-                            builder.append(m[i]);
-                            builder.append("*x^");
-                            builder.append(i);
-                        } else {
-                            builder.append("x^");
-                            builder.append(i);
-                        }
-                    }
-                }
-            }
-        }
-        if (flag) {
-            builder.append(0);
-        }
-        return builder.toString();
-    }
-
-    HashMap<Integer, List<Integer>> getFactorRing(List<Integer> ideal, int[][] sum) {
-        HashMap<Integer, List<Integer>> factorRing = new HashMap<>();
-        int[] ring = new int[sum.length];
-        for (int i = 0; i < sum.length; i++) {
-            ring[i] = i;
-        }
-        boolean[] used = new boolean[sum.length];
-        for (int i = 0; i < sum.length; i++) {
-            if (!used[i]) {
-                used[i] = true;
-                for (int j = 0; j < sum.length; j++) {
-                    if (ideal.contains(sum[i][getJ(ring.length, j)])) {
-                        if (!factorRing.containsKey(i)) {
-                            factorRing.put(i, new ArrayList<>());
-                        }
-                        factorRing.get(i).add(j);
-                        used[j] = true;
-                    }
-                }
-            }
-        }
-        return factorRing;
-    }
-
-    List<Integer> getIdeal(int[] genSet, int[][] sum, int[][] mult) {
-        List<Integer> ideal = new ArrayList<>();
-        for (int i = 0; i < genSet.length; i++) {
-            ideal.add(genSet[i]);
-        }
-        int[] ring = new int[sum.length];
-        for (int i = 0; i < sum.length; i++) {
-            ring[i] = i;
-        }
-        int n;
-        do {
-            n = ideal.size();
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    int element = sum[i][getJ(ring.length, j)];
-                    if (element != -1 && !ideal.contains(element)) {
-                        ideal.add(element);
-                    }
-                    element = mult[i][ring[j]];
-                    if (!ideal.contains(element)) {
-                        ideal.add(element);
-                    }
-                }
-            }
-        } while (n != ideal.size());
-        return ideal;
-    }
-
-    int getJ(int j, int n) {
-        for (int i = 0; i < n; i++) {
-            if ((i + j) % n == 0) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    List<Integer> getSubRing(int[] genSet, int[][] sum, int[][] mult) {
-        List<Integer> subRing = new ArrayList<>();
-        for (int i = 0; i < genSet.length; i++) {
-            subRing.add(genSet[i]);
-        }
-        int n;
-        do {
-            n = subRing.size();
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    int element = sum[i][j];
-                    if (!subRing.contains(element)) {
-                        subRing.add(element);
-                    }
-                    element = mult[i][j];
-                    if (!subRing.contains(element)) {
-                        subRing.add(element);
-                    }
-                }
-            }
-        } while (n != subRing.size());
-        return subRing;
-    }
-
     boolean isRing(int[][] sum, int[][] mult) {
         if (!isСommutative(sum) || !isAssociative(sum) ||
                 !hasOppositeElementAdd(sum) || !hasNeutralElementAdd(sum) ||
@@ -552,5 +290,267 @@ public class Main {
             }
         }
         return true;
+    }
+
+    List<Integer> getSubRing(int[] genSet, int[][] sum, int[][] mult) {
+        List<Integer> subRing = new ArrayList<>();
+        for (int i = 0; i < genSet.length; i++) {
+            subRing.add(genSet[i]);
+        }
+        int n;
+        do {
+            n = subRing.size();
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    int element = sum[i][j];
+                    if (!subRing.contains(element)) {
+                        subRing.add(element);
+                    }
+                    element = mult[i][j];
+                    if (!subRing.contains(element)) {
+                        subRing.add(element);
+                    }
+                }
+            }
+        } while (n != subRing.size());
+        return subRing;
+    }
+
+    List<Integer> getIdeal(int[] genSet, int[][] sum, int[][] mult) {
+        List<Integer> ideal = new ArrayList<>();
+        for (int i = 0; i < genSet.length; i++) {
+            ideal.add(genSet[i]);
+        }
+        int[] ring = new int[sum.length];
+        for (int i = 0; i < sum.length; i++) {
+            ring[i] = i;
+        }
+        int n;
+        do {
+            n = ideal.size();
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    int element = sum[i][getJ(ring.length, j)];
+                    if (element != -1 && !ideal.contains(element)) {
+                        ideal.add(element);
+                    }
+                    element = mult[i][ring[j]];
+                    if (!ideal.contains(element)) {
+                        ideal.add(element);
+                    }
+                }
+            }
+        } while (n != ideal.size());
+        return ideal;
+    }
+
+    HashMap<Integer, List<Integer>> getFactorRing(List<Integer> ideal, int[][] sum) {
+        HashMap<Integer, List<Integer>> factorRing = new HashMap<>();
+        int[] ring = new int[sum.length];
+        for (int i = 0; i < sum.length; i++) {
+            ring[i] = i;
+        }
+        boolean[] used = new boolean[sum.length];
+        for (int i = 0; i < sum.length; i++) {
+            if (!used[i]) {
+                used[i] = true;
+                for (int j = 0; j < sum.length; j++) {
+                    if (ideal.contains(sum[i][getJ(ring.length, j)])) {
+                        if (!factorRing.containsKey(i)) {
+                            factorRing.put(i, new ArrayList<>());
+                        }
+                        factorRing.get(i).add(j);
+                        used[j] = true;
+                    }
+                }
+            }
+        }
+        return factorRing;
+    }
+
+    int getJ(int j, int n) {
+        for (int i = 0; i < n; i++) {
+            if ((i + j) % n == 0) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    String printPolynomial(int[] m) {
+        StringBuilder builder = new StringBuilder();
+        boolean flag = true;
+        for (int i = 0; i < m.length; i++) {
+            if (m[i] != 0) {
+                if (i > 0 && !flag) {
+                    builder.append("+");
+                }
+                flag = false;
+                if (i == 0) {
+                    builder.append(m[i]);
+                } else {
+                    if (i == 1) {
+                        if (m[i] > 1) {
+                            builder.append(m[i]);
+                            builder.append("*");
+                        }
+
+                        builder.append("x");
+                    } else {
+                        if (m[i] > 1) {
+                            builder.append(m[i]);
+                            builder.append("*x^");
+                            builder.append(i);
+                        } else {
+                            builder.append("x^");
+                            builder.append(i);
+                        }
+                    }
+                }
+            }
+        }
+        if (flag) {
+            builder.append(0);
+        }
+        return builder.toString();
+    }
+
+    List<int[]> getElementsExtention(int p, int[] m) {
+        int amount = (int) Math.pow(p, m.length - 1);
+        List<int[]> expansion = new ArrayList<>();
+        int[] polynomial = new int[m.length];
+        for (int i = 0; i < amount; i++) {
+            expansion.add(polynomial);
+            polynomial = getPolynomial(polynomial, p);
+        }
+        return expansion;
+    }
+
+    int[] getPolynomial(int[] m, int p) {
+        int[] polynomial = m.clone();
+        for (int i = 0; i < polynomial.length; i++) {
+            if (polynomial[i] == p - 1) {
+                polynomial[i] = 0;
+            } else {
+                polynomial[i]++;
+                break;
+            }
+        }
+        return polynomial;
+    }
+
+    int[] add(int[] x, int[] y, int p) {
+        List<Integer> z = new ArrayList<>();
+        if (x.length > y.length) {
+            for (int aX : x) {
+                z.add(aX);
+            }
+        } else {
+            for (int aY : y) {
+                z.add(aY);
+            }
+        }
+        for (int i = 0; i < z.size(); i++) {
+            z.set(i, (x[i] + y[i]) % p);
+        }
+        while (z.size() > 1 && z.get(z.size() - 1) == 0) {
+            z.remove(z.size() - 1);
+        }
+        int[] result = new int[z.size()];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = z.get(i);
+        }
+        return result;
+    }
+
+    int[] mult(int[] x, int[] y, int p) {
+        ArrayList<Integer> z = new ArrayList<>();
+        for (int i = 0; i < x.length + y.length; i++) {
+            z.add(0);
+        }
+        for (int i = 0; i < x.length; i++) {
+            for (int j = 0; j < y.length; j++) {
+                z.set(i + j, (z.get(i + j) + ((x[i] * y[j]) % p)) % p);
+            }
+        }
+        while (z.size() > 1 && z.get(z.size() - 1) == 0) {
+            z.remove(z.size() - 1);
+        }
+
+        int[] result = new int[z.size()];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = z.get(i);
+        }
+        return result;
+    }
+
+    int[] mod(int[] x, int[] y, int p) {
+        ArrayList<Integer> z = new ArrayList<>();
+        for (int aX : x) {
+            z.add(aX);
+        }
+        if (x.length < y.length || extendedEuclid(y[y.length - 1], p) != 1) {
+            int[] result = new int[z.size()];
+            for (int i = 0; i < result.length; i++) {
+                result[i] = z.get(i);
+            }
+            return result;
+        }
+        int degX = x.length - 1;
+        int degY = y.length - 1;
+        int inverse = modInverse(y[y.length - 1], p);
+        while (z.size() >= y.length) {
+            int degZ = degX - degY;
+            int element = (inverse * z.get(degX)) % p;
+            for (int i = 0; i < degY + 1; i++) {
+                z.set(i + degZ, (z.get(i + degZ) - y[i] * element) % p);
+                if (z.get(i + degZ) < 0) {
+                    z.set(i + degZ, z.get(i + degZ) + p);
+                }
+            }
+            while (z.size() > 1 && z.get(z.size() - 1) == 0) {
+                z.remove(z.size() - 1);
+            }
+            degX = z.size() - 1;
+            if (z.get(z.size() - 1) == 0) {
+                break;
+            }
+        }
+        while (z.size() > 1 && z.get(z.size() - 1) == 0) {
+            z.remove(z.size() - 1);
+        }
+        int[] result = new int[z.size()];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = z.get(i);
+        }
+        return result;
+    }
+
+    int modInverse(int x, int p) {
+        return (extendedEuclid(x, p) % p + p) % p;
+    }
+
+    int extendedEuclid(int a, int b) {
+        int x, y;
+        int r0 = a, r1 = b, x0 = 1, x1 = 0, y0 = 0, y1 = 1;
+        while (true) {
+            int q = r0 / r1;
+            int r = r0 % r1;
+            if (r == 0)
+                break;
+            else {
+                r0 = r1;
+                r1 = r;
+                x = x0 - q * x1;
+                x0 = x1;
+                x1 = x;
+                y = y0 - q * y1;
+                y0 = y1;
+                y1 = y;
+            }
+        }
+        x = x1;
+        y = y1;
+        return x;
     }
 }
